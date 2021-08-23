613e413c7ebc27f80ee54a5c010fdfc205a79873
Brandon <bjkrynicki@gmail.com>
2021-08-23 14:41:39 -0500

    commit tree b12ac5d54c65d23caa19a486fbe3d7d77da87490

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+//Define currentUser
+const currentUser = `Grace Hopper`;
+
+//Standard Welcome Message
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage);
+
+//Excited Welcome Message
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage)
+
+//Short Greeting Message
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+console.log(shortGreeting);
